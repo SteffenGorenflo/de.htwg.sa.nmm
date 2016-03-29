877eb2a6c6b636c968e5a3204e5851a64e2d85db
Patrick Schmidt <schmidt.patrick.14@gmail.com>
2016-03-29 14:08:04 +0200

    implemented db4o methods

--- src/de/htwg/sa/nmm/persistence/db4o/db4oDao.java
+++ src/de/htwg/sa/nmm/persistence/db4o/db4oDao.java
@@ -1,36 +1,81 @@
 package de.htwg.sa.nmm.persistence.db4o;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.db4o.Db4o;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Predicate;
+
+import de.htwg.sa.nmm.model.IGamefield;
 import de.htwg.sa.nmm.persistence.IDAO;
 import de.htwg.sa.nmm.persistence.OperationResult;
-import de.htwg.sa.nmm.model.IGamefield;
 
 public class db4oDao implements IDAO {
 
-	@Override
-	public OperationResult storeGamefield(IGamefield gamefield) {
-		// TODO Auto-generated method stub
-		return null;
+	private static final String FILENAME = "nmm_db4o.db";
+
+	private ObjectContainer db = null;
+
+	public db4oDao() {
+		db = Db4o.openFile(FILENAME);
+		System.out.println("db open");
 	}
 
 	@Override
-	public IGamefield loadGamefiledById(String id) {
-		// TODO Auto-generated method stub
+	public OperationResult storeGamefield(IGamefield gamefield) {
+		if (null != loadGamefiledById(gamefield.getId())) {
+			deleteGamefieldById(gamefield.getId());
+		}
+		try {
+			System.out.println("Spiel wird gespeichert");
+			db.store(gamefield);
+		} catch (Exception e) {
+			return new OperationResult(false, e.getMessage());
+		}
+		return new OperationResult(true);
+	}
+
+	@Override
+	public IGamefield loadGamefiledById(final String id) {
+		ObjectSet<IGamefield> game = db.query(new Predicate<IGamefield>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean match(IGamefield game) {
+				return game.getId().equals(id);
+			}
+		});
+		
+		System.out.println(game);
+		if (!game.isEmpty())
+			return game.get(0);
 		return null;
 	}
 
 	@Override
 	public List<String> getAllGamefieldIds() {
-		// TODO Auto-generated method stub
-		return null;
+		// Get all stored Games
+		ObjectSet<IGamefield> games = db.query(IGamefield.class);
+
+		// Extract all GameIds
+		List<String> gameIds = new ArrayList<String>();
+		for (IGamefield game : games) {
+			gameIds.add(game.getId());
+		}
+		return gameIds;
 	}
 
 	@Override
 	public OperationResult deleteGamefieldById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			db.delete(loadGamefiledById(id));
+		} catch (Exception e){
+			return new OperationResult(false, e.getMessage());
+		}
+		return new OperationResult(true);
 	}
 
-	
 }
