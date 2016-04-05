551e054c4e71de1577ffb51c7f57aa2fd937ac17
Patrick Schmidt <schmidt.patrick.14@gmail.com>
2016-04-05 13:32:05 +0200

    added javadoc

--- src/de/htwg/sa/nmm/model/IPlayer.java
+++ src/de/htwg/sa/nmm/model/IPlayer.java
@@ -4,8 +4,20 @@ import java.awt.Color;
 
 import de.htwg.sa.nmm.model.IToken;
 
+/**
+ * Interface for a player
+ * 
+ * @author Markus Heilig
+ * @since 2014-05-30
+ */
 public interface IPlayer {
 	
+	/**
+	 * Current status of the player
+	 * 
+	 * @author Markus Heilig
+	 * @since 2014-05-30
+	 */
 	enum Status {
 		SetToken,
 		MoveToken,
